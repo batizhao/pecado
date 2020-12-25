43a961c7b0348dab244801c0c4c29b2eb49d04ea
batizhao <zhaobati@gmail.com>
2020-12-25 17:15:44 +0800

    gateway disable security test.

--- pecado-gateway/src/test/java/me/batizhao/gateway/integration/BaseIntegrationTest.java
+++ pecado-gateway/src/test/java/me/batizhao/gateway/integration/BaseIntegrationTest.java
@@ -1,22 +1,15 @@
 package me.batizhao.gateway.integration;
 
 import me.baitzhao.gateway.PecadoGatewayApplication;
-import me.batizhao.common.core.exception.WebExceptionHandler;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.SocketUtils;
 
 import java.time.Duration;
 
@@ -28,7 +21,7 @@ import java.time.Duration;
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = PecadoGatewayApplication.class)
-@Import(WebExceptionHandler.class)
+//@Import(WebExceptionHandler.class)
 @ActiveProfiles("integration")
 @Tag("integration")
 public abstract class BaseIntegrationTest {
